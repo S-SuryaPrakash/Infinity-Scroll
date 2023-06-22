434298a3a806c7aabb6c056ac4055234d58bbe2c
S.Surya Prakash <s.suryaprakash7767@gmail.com>
2023-06-22 22:26:49 +0530

    Added the functionalities

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-const imagContainer=document.getElementById('image-container');
+const imageContainer=document.getElementById('image-container');
 const loader=document.getElementById('loader');
+
+let ready=false;
+let imagesLoaded=0;
+let totalImages=0;
 
 let photosArray=[];
 
-const count=10;
+const count=30;
 const apiKey='mw1XjlzOIBkei0W-8w2HwOYaoLbzBeGb9aeIxwckvd4';
 const apiUrl=`https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+function imageLoaded(){
+    imagesLoaded++;
+    if(imagesLoaded===totalImages){
+        loader=true;
+        ready=true;
+        console.log('ready=',ready)
+    }
+}
+
+
 // Create elements for links, Photos and add to DOM
 function displayPhotos(){
+    imagesLoaded=0;
+    totalImages=photosArray.length;
     photosArray.forEach((photo)=>{
         // Create an anchor element to link to unsplash
         const item=document.createElement('a');
@@ -17,13 +33,15 @@ function displayPhotos(){
 
         // Create <img> for photo
         const img= document.createElement('img')
-        img.setAttribute('src',photos.urls.regular);
+        img.setAttribute('src',photo.urls.regular);
         img.setAttribute('alt',photo.alt_description);
         img.setAttribute('title',photo.alt_description)
 
+        img.addEventListener('load',imageLoaded)
+
         // Put image inside anchor tag then put both inside image container element
         item.appendChild(img)
-        imagContainer.appendChild(item)
+        imageContainer.appendChild(item)
     });
 }
 
@@ -32,9 +50,21 @@ async function getPhotos(){
     try{
         const response=await fetch(apiUrl);
         photosArray=await response.json();
+        // console.log(photosArray);
         displayPhotos();
     }
     catch(error){
-
+        getPhotos();
     }
 }
+
+// Check to see if scrolling near bottom of page ,Load more photos
+window.addEventListener('scroll',()=>{
+    if(window.innerHeight + window.scrollY>=document.body.offsetHeight - 1000){
+        getPhotos();
+    }
+});
+
+
+// On load
+getPhotos();
